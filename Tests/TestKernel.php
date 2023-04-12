4e1cf1cd8e68ec937809021e342f766005706a9a
Douglas Posner <dposner@dayspringpartners.com>
2023-04-12 09:16:51 -0700

    remove swiftmailer from kernel test

--- Tests/TestKernel.php
+++ Tests/TestKernel.php
@@ -17,7 +17,7 @@ class TestKernel extends Kernel
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
-            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+//            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
 //            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 //            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
